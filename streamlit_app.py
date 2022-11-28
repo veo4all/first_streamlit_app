a56066258ce48d128f6a2ce022994efbca48a252
veo4all <33179209+veo4all@users.noreply.github.com>
2022-11-27 20:56:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit
 
 streamlit.title('New Healty Diner')
 
-
-streamlit.header('Menu')
-streamlit.text('item 1')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')

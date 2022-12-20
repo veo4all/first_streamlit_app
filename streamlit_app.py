3d6c7bfeed0b1d1386b2e8085c9e62d67235eafd
veo4all <33179209+veo4all@users.noreply.github.com>
2022-12-19 20:44:43 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,6 +60,7 @@ if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()    
     streamlit.dataframe(my_data_rows)   
+
     
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 #my_data_row = my_cur.fetchone()
@@ -70,10 +71,21 @@ if streamlit.button('Get Fruit Load List'):
 #my_data_rows = my_cur.fetchall()
 #streamlit.dataframe(my_data_rows)
 
-streamlit.stop()
+#streamlit.stop()
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into fruit_load_list values('from streamlit')")
+        return "Thanks for adding " + new_fruit
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
 
-my_cur.execute("insert into fruit_load_list values('from streamlit')")
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add Fruit to the List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    res = insert_row_snowflake(add_my_fruit)
+    streamlit.text(res)
+    
+
+
+
+
 

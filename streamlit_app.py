34b1055402fb65094434555fab42acdff296219b
veo4all <33179209+veo4all@users.noreply.github.com>
2022-12-19 20:21:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,19 +26,24 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+#fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+#streamlit.write('The user entered ', fruit_choice)
 ## streamlit.text(fruityvice_response.json())
 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
 
-streamlit.dataframe(fruityvice_normalized)
-
+  if not fruit_choice:
+    streamlit.error("Select a fruit.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+    
+except URLError as e:
+  streamlit.error()
+  
 streamlit.stop()
-
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()

c758653c952ad5fa2ba11db581fcefb5e83f7059
veo4all <33179209+veo4all@users.noreply.github.com>
2022-12-19 20:48:48 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,7 +58,8 @@ def get_fruit_load_list():
 
 if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    my_data_rows = get_fruit_load_list()    
+    my_data_rows = get_fruit_load_list()  
+    my_cnx.close()
     streamlit.dataframe(my_data_rows)   
 
     
@@ -74,7 +75,7 @@ if streamlit.button('Get Fruit Load List'):
 #streamlit.stop()
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("insert into fruit_load_list values('from streamlit')")
+        my_cur.execute("insert into fruit_load_list values('" + new_fruit + "')")
         return "Thanks for adding " + new_fruit
 
 

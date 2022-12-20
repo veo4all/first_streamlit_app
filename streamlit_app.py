3bed081e0732bf48b90d793a857cb958aa2569bd
veo4all <33179209+veo4all@users.noreply.github.com>
2022-12-19 20:29:10 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 #streamlit.write('The user entered ', fruit_choice)
 ## streamlit.text(fruityvice_response.json())
 
-def get_fruityvice_data(this_fruit_choice)
+def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized

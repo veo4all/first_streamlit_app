9f9ee9503eff29f0110862e10003538471f32631
veo4all <33179209+veo4all@users.noreply.github.com>
2022-12-19 20:28:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,15 +30,19 @@ streamlit.header("Fruityvice Fruit Advice!")
 #streamlit.write('The user entered ', fruit_choice)
 ## streamlit.text(fruityvice_response.json())
 
+def get_fruityvice_data(this_fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+  
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
 
   if not fruit_choice:
     streamlit.error("Select a fruit.")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
+    df = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(df)
     
 except URLError as e:
   streamlit.error()
